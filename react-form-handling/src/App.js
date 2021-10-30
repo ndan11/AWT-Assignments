ddbbd8c2e715775825b0955afb9b96cf03fe7f9a
Nandan <59531496+ndan11@users.noreply.github.com>
2021-10-30 17:09:52 +0530

    Inital Commit

---
+++ react-form-handling/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SignupForm from './components/SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to React Application
+      <SignupForm/>
+    </div>
+  );
+}
+
+export default App;
